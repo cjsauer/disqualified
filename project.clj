0d3988575527cca877450b57b0509f7d87f16bb0
Calvin Sauer <calvin.j.sauer@gmail.com>
2018-06-13 22:31:28 -0400

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cjsauer/mass-transit "0.1.0"
+  :description "Tiny Clojure(Script) library for converting between qualified and unqualified keyword maps"
+  :url "http://github.com/cjsauer/mass-transit"
+  :dependencies []
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :test {:source-paths ["test"]}})
