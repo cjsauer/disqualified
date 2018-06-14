333d2483b779f8c1f24ede56066725930171b637
Calvin Sauer <calvin.j.sauer@gmail.com>
2018-06-13 22:53:40 -0400

    Update scm url

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject cjsauer/disqualified "0.1.0"
+(defproject cjsauer/disqualified "0.1.1-SNAPSHOT"
   :description "Tiny Clojure(Script) library for converting between qualified and unqualified keyword maps"
   :url "http://github.com/cjsauer/disqualified"
+  :scm {:name "git"
+        :url "http://github.com/cjsauer/disqualified"}
   :dependencies []
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :test {:source-paths ["test"]}})
